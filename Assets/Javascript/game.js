91cb740b01c880409e55105800b17543474f08d2
Ashley Clark <debashley06@gmail.com>
2019-09-11 18:07:59 -0400

    html css java pages for crystal game

---
+++ Assets/Javascript/game.js
@@ -0,0 +1,6 @@
+var crystal1 = document.getElementById("crystal1");
+var crystal2 = document.getElementById("crystal2");
+var crystal3 = document.getElementById("crystal3");
+var crystal4 = document.getElementById("crystal4");
+
+var random = document.getElementsByClassName("random");
